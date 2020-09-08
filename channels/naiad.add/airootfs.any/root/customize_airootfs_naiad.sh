7c126f5f68febdf4b8f93d5e8ac035d1eb2db7eb
nexryai <nexryai@gmail.com>
2020-09-08 21:01:58 +0900

    各種バグの修正

---
+++ channels/naiad.add/airootfs.any/root/customize_airootfs_naiad.sh
@@ -0,0 +1,121 @@
+#!/usr/bin/env bash
+#
+
+
+set -e -u
+
+
+# Default value
+# All values can be changed by arguments.
+password=alter
+boot_splash=false
+kernel='zen'
+theme_name=alter-logo
+rebuild=false
+japanese=false
+username='alter'
+os_name="Alter Linux"
+install_dir="alter"
+usershell="/bin/bash"
+debug=true
+
+
+# Parse arguments
+while getopts 'p:bt:k:rxju:o:i:s:da:' arg; do
+    case "${arg}" in
+        p) password="${OPTARG}" ;;
+        b) boot_splash=true ;;
+        t) theme_name="${OPTARG}" ;;
+        k) kernel="${OPTARG}" ;;
+        r) rebuild=true ;;
+        j) japanese=true;;
+        u) username="${OPTARG}" ;;
+        o) os_name="${OPTARG}" ;;
+        i) install_dir="${OPTARG}" ;;
+        s) usershell="${OPTARG}" ;;
+        d) debug=true ;;
+        x) debug=true; set -xv ;;
+        a) arch="${OPTARG}"
+    esac
+done
+
+
+# Delete file only if file exists
+# remove <file1> <file2> ...
+function remove () {
+    local _list
+    local _file
+    _list=($(echo "$@"))
+    for _file in "${_list[@]}"; do
+        if [[ -f ${_file} ]]; then
+            rm -f "${_file}"
+        elif [[ -d ${_file} ]]; then
+            rm -rf "${_file}"
+        fi
+        echo "${_file} was deleted."
+    done
+}
+
+
+# Replace wallpaper.
+if [[ -f /usr/share/backgrounds/xfce/xfce-stripes.png ]]; then
+    remove /usr/share/backgrounds/xfce/xfce-stripes.png
+    ln -s /usr/share/backgrounds/naiad.png /usr/share/backgrounds/xfce/xfce-stripes.png
+fi
+[[ -f /usr/share/backgrounds/naiad.png ]] && chmod 644 /usr/share/backgrounds/naiad.png
+
+
+# Bluetooth
+rfkill unblock all
+systemctl enable bluetooth
+
+# Snap
+if [[ "${arch}" = "x86_64" ]]; then
+    systemctl enable snapd.apparmor.service
+    systemctl enable apparmor.service
+    systemctl enable snapd.socket
+    systemctl enable snapd.service
+fi
+
+
+# Update system datebase
+dconf update
+
+
+# firewalld
+systemctl enable firewalld.service
+
+
+# Replace right menu
+if [[ "${japanese}" = true ]]; then
+    remove "/etc/skel/.config/Thunar/uca.xml"
+    remove "/home/${username}/.config/Thunar/uca.xml"
+
+    mv "/etc/skel/.config/Thunar/uca.xml.jp" "/etc/skel/.config/Thunar/uca.xml"
+    mv "/home/${username}/.config/Thunar/uca.xml.jp" "/home/${username}/.config/Thunar/uca.xml"
+else
+    remove "/etc/skel/.config/Thunar/uca.xml.jp"
+    remove "/home/${username}/.config/Thunar/uca.xml.jp"
+fi
+
+# Added autologin group to auto login
+groupadd autologin
+usermod -aG autologin ${username}
+
+# ntp
+systemctl enable systemd-timesyncd.service
+
+
+# Enable LightDM to auto login
+if [[ "${boot_splash}" =  true ]]; then
+    systemctl enable lightdm-plymouth.service
+else
+    systemctl enable lightdm.service
+fi
+
+
+# Set script permission
+chmod 755 /usr/bin/alterlinux-gtk-bookmarks
+
+# Replace auto login user
+sed -i s/%USERNAME%/${username}/g /etc/lightdm/lightdm.conf
