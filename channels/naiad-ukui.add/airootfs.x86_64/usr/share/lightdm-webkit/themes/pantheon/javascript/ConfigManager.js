1f953e4d91a1c3354a5cdfab84136c1b3d27f7df
nexryai <nexryai@gmail.com>
2020-11-15 07:43:16 +0900

    theshell、ukuiチャンネルを廃止して試験的なproチャンネルを追加

--- channels/naiad-ukui.add/airootfs.x86_64/usr/share/lightdm-webkit/themes/pantheon/javascript/ConfigManager.js
+++
@@ -1,9 +0,0 @@
-var ConfigManager = function(greeter) {
-	this.greeter	= greeter;
-
-	if (typeof Config == 'undefined')
-		return;
-	if (Config.background) {
-		$('body').css('background', "url('" + Config.background + "') no-repeat black").css('background-size', 'cover');
-	}
-};
